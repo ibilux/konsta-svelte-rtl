eabcdfc20254a9016a948d0bb913a8ce24441719
ibilux <i.bilux@gmail.com>
2023-03-28 12:19:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# konsta-svelte-rtl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/konsta-svelte-1tlda1)
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const tailwindMobile = require('konsta/config');
+
+module.exports = tailwindMobile({
+  content: [
+    './components/**/*.{js,jsx,ts,tsx,svelte}',
+    './pages/**/*.{js,jsx,ts,tsx,svelte}',
+    './*.svelte',
+    './*.js',
+    './index.html',
+  ],
+  konsta: {
+    colors: {
+      primary: '#007aff',
+      'brand-primary': '#007aff',
+      'brand-red': '#ff3b30',
+      'brand-green': '#4cd964',
+      'brand-yellow': '#ffcc00',
+      'brand-purple': '#9c27b0',
+      'brand-blue': '#2196f3',
+    },
+  },
+  darkMode: 'class',
+});
